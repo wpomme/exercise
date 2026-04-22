ee95159e437e5e5087d63ed387be4c8f7cce4ef4
yuya hashimoto <ne25qc3b@gmail.com>
2026-04-22 12:32:49 +0900

    Run lint to fizz_buzz.rb

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ rubocop -a <filename>
 # 全て修正
 rubocop -A <filename>
 ```
+
+# TODO
+Ruby を4.0.2に更新したらMiniTest -> Minitestとなったので次の対応が必要
+```bash
+sed -i "" s/MiniTest::Test/Minitest::Test/g <filename>
+```
--- test-modules-by-ruby/test/fizz_buzz_test.rb
+++ test-modules-by-ruby/test/fizz_buzz_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
-require_relative '../lib/fizz_buzz.rb'
+require_relative '../lib/fizz_buzz'
 
-class FizzBuzzTest < MiniTest::Test
+class FizzBuzzTest < Minitest::Test
   def setup
     # 適当な素数のリストを作成する
     @test_prime_nums = [83, 883, 4993]
@@ -9,21 +9,21 @@ class FizzBuzzTest < MiniTest::Test
 
   def test_fizz_buzz_with_prime_number
     # fizz_buzzに与えた数値が3の倍数でも5の倍数でもなければ、そのまま数値を返す
-    assert_equal @test_prime_nums.map(&:to_s), @test_prime_nums.map { |i| fizz_buzz(i) }
+    assert_equal(@test_prime_nums.map(&:to_s), @test_prime_nums.map { |i| fizz_buzz(i) })
   end
 
   def test_fizz_buzz_with_a_multiple_of_three
     # fizz_buzzに与えた数値が3の倍数なら、'Fizz'を返す
-    assert_equal (1..3).map {"Fizz"}, @test_prime_nums.map { |i| fizz_buzz(i*3) }
+    assert_equal((1..3).map { "Fizz" }, @test_prime_nums.map { |i| fizz_buzz(i * 3) })
   end
 
   def test_fizz_buzz_with_a_multiple_of_five
     # fizz_buzzに与えた数値が5の倍数なら、'Buzz'を返す
-    assert_equal (1..3).map {"Buzz"}, @test_prime_nums.map { |i| fizz_buzz(i*5) }
+    assert_equal((1..3).map { "Buzz" }, @test_prime_nums.map { |i| fizz_buzz(i * 5) })
   end
 
   def test_fizz_buzz_with_a_multiple_of_fifteen
     # fizz_buzzに与えた数値が15の倍数なら、'Fizz Buzz'を返す
-    assert_equal (1..3).map {"Fizz Buzz"}, @test_prime_nums.map { |i| fizz_buzz(i*15) }
+    assert_equal((1..3).map { "Fizz Buzz" }, @test_prime_nums.map { |i| fizz_buzz(i * 15) })
   end
 end
